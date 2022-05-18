186fa0ae85f5384dbb0c338830f7eb664cb36119
AlbertWebs <albertmuhatia@gmail.com>
2022-05-18 17:19:46 +0300

    Update Alt Texts

--- resources/views/front/index.blade.php
+++ resources/views/front/index.blade.php
@@ -114,7 +114,7 @@
                <div class="projects-masonry__item __js_masonry-item  col-12 col-md-6 col-lg-4 col-xl-3" data-order="1">
                   <div class="preview-card">
                      <div class="preview-card__image height-428">
-                        <img src="{{asset('theme/uploads/interiors/photo_2021-01-15_07-00-44.jpg')}}" srcset="uploads/interiors/photo_2021-01-15_07-00-44.jpg 2x')}}" width="428" height="428" alt="">
+                        <img src="{{asset('theme/uploads/interiors/photo_2021-01-15_07-00-44.jpg')}}" srcset="uploads/interiors/photo_2021-01-15_07-00-44.jpg 2x')}}" width="428" height="428" alt="Jspex Design">
                      </div>
                      <div class="preview-card__content">
                         <h3 class="preview-card__heading">Interior Design</h3>
@@ -131,7 +131,7 @@
                <div class="projects-masonry__item __js_masonry-item  col-12 col-md-6 col-lg-4 col-xl-3" data-order="2">
                   <div class="preview-card">
                      <div class="preview-card__image height-428">
-                        <img src="{{asset('theme/uploads/interiors/photo_2021-01-15_07-00-37.jpg')}}" srcset="uploads/interiors/photo_2021-01-15_07-00-37.jpg 2x')}}" width="428" height="428" alt="">
+                        <img src="{{asset('theme/uploads/interiors/photo_2021-01-15_07-00-37.jpg')}}" srcset="uploads/interiors/photo_2021-01-15_07-00-37.jpg 2x')}}" width="428" height="428" alt="Jspex Design">
                      </div>
                      <div class="preview-card__content">
                         <h3 class="preview-card__heading">Interior Design</h3>
@@ -148,7 +148,7 @@
                <div class="projects-masonry__item __js_masonry-item  col-12 col-md-6 col-lg-4 col-xl-3" data-order="3">
                   <div class="preview-card">
                      <div class="preview-card__image height-428">
-                        <img src="{{asset('theme/uploads/interiors/photo_2021-03-28_11-34-16.jpg')}}" srcset="uploads/interiors/photo_2021-03-28_11-34-16.jpg 2x')}}" width="428" height="428" alt="">
+                        <img src="{{asset('theme/uploads/interiors/photo_2021-03-28_11-34-16.jpg')}}" srcset="uploads/interiors/photo_2021-03-28_11-34-16.jpg 2x')}}" width="428" height="428" alt="Jspex Design">
                      </div>
                      <div class="preview-card__content">
                         <h3 class="preview-card__heading">Interior Design</h3>
@@ -167,7 +167,7 @@
                <div class="projects-masonry__item __js_masonry-item  col-12  col-lg-6" data-order="4">
                   <div class="preview-card">
                      <div class="preview-card__image height-428">
-                        <img src="{{asset('theme/uploads/interiors/photo_2022-04-18_19-45-40.jpg')}}" srcset="uploads/interiors/photo_2022-04-18_19-45-40.jpg 2x')}}" width="886" height="428" alt="">
+                        <img src="{{asset('theme/uploads/interiors/photo_2022-04-18_19-45-40.jpg')}}" srcset="uploads/interiors/photo_2022-04-18_19-45-40.jpg 2x')}}" width="886" height="428" alt="Jspex Design">
                      </div>
                      <div class="preview-card__content">
                         <h3 class="preview-card__heading">Landscaping</h3>
@@ -186,7 +186,7 @@
                <div class="projects-masonry__item __js_masonry-item  col-12 col-md-6 col-lg-4 col-xl-3" data-order="5">
                   <div class="preview-card">
                      <div class="preview-card__image 886">
-                        <img src="{{asset('theme/uploads/interiors/photo_2022-04-18_19-30-39.jpg')}}" srcset="uploads/interiors/photo_2022-04-18_19-30-39.jpg 2x')}}" width="428" height="886" alt="">
+                        <img src="{{asset('theme/uploads/interiors/photo_2022-04-18_19-30-39.jpg')}}" srcset="uploads/interiors/photo_2022-04-18_19-30-39.jpg 2x')}}" width="428" height="886" alt="Jspex Design">
                      </div>
                      <div class="preview-card__content">
                         <h3 class="preview-card__heading">Landscaping</h3>
@@ -204,7 +204,7 @@
                <div class="projects-masonry__item __js_masonry-item  col-12 col-md-6 col-lg-4 col-xl-3" data-order="6">
                   <div class="preview-card">
                      <div class="preview-card__image height-428">
-                        <img src="{{asset('theme/img/picture/home-video/6.jpg')}}" srcset="{{asset('theme/img/picture/home-video/6@2x.jpg 2x')}}" width="428" height="428" alt="">
+                        <img src="{{asset('theme/img/picture/home-video/6.jpg')}}" srcset="{{asset('theme/img/picture/home-video/6@2x.jpg 2x')}}" width="428" height="428" alt="Jspex Design">
                      </div>
                      <div class="preview-card__content">
                         <h3 class="preview-card__heading">Interior Design</h3>
@@ -221,7 +221,7 @@
                <div class="projects-masonry__item __js_masonry-item  col-12  col-lg-6" data-order="7">
                   <div class="preview-card">
                      <div class="preview-card__image height-428">
-                        <img src="{{asset('theme/uploads/interiors/photo_2021-01-13_14-38-54.jpg')}}" srcset="uploads/interiors/photo_2021-01-13_14-38-54.jpg 2x')}}" width="886" height="428" alt="">
+                        <img src="{{asset('theme/uploads/interiors/photo_2021-01-13_14-38-54.jpg')}}" srcset="uploads/interiors/photo_2021-01-13_14-38-54.jpg 2x')}}" width="886" height="428" alt="Jspex Design">
                      </div>
                      <div class="preview-card__content">
                         <h3 class="preview-card__heading">Interior Design</h3>
@@ -238,7 +238,7 @@
                <div class="projects-masonry__item __js_masonry-item  col-12 col-md-6 col-lg-4 col-xl-3" data-order="8">
                   <div class="preview-card">
                      <div class="preview-card__image height-428">
-                        <img src="{{asset('theme/img/picture/home-video/7.jpg')}}" srcset="{{asset('theme/img/picture/home-video/7@2x.jpg 2x')}}" width="428" height="428" alt="">
+                        <img src="{{asset('theme/img/picture/home-video/7.jpg')}}" srcset="{{asset('theme/img/picture/home-video/7@2x.jpg 2x')}}" width="428" height="428" alt="Jspex Design">
                      </div>
                      <div class="preview-card__content">
                         <h3 class="preview-card__heading">Interior Design</h3>
@@ -255,7 +255,7 @@
                <div class="projects-masonry__item __js_masonry-item  col-12 col-md-6 col-lg-4 col-xl-3" data-order="9">
                   <div class="preview-card">
                      <div class="preview-card__image height-428">
-                        <img src="{{asset('theme/img/picture/home-video/8.jpg')}}" srcset="{{asset('theme/img/picture/home-video/8@2x.jpg 2x')}}" width="428" height="428" alt="">
+                        <img src="{{asset('theme/img/picture/home-video/8.jpg')}}" srcset="{{asset('theme/img/picture/home-video/8@2x.jpg 2x')}}" width="428" height="428" alt="Jspex Design">
                      </div>
                      <div class="preview-card__content">
                         <h3 class="preview-card__heading">Interior Design</h3>
