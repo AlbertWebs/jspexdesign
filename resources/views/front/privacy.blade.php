fc7dd8aeed5ca61a607eb75963606f03ee512d72
AlbertWebs <albertmuhatia@gmail.com>
2022-05-21 09:41:09 +0300

    Update Terms

--- resources/views/front/privacy.blade.php
+++ resources/views/front/privacy.blade.php
@@ -3,24 +3,26 @@
 
 <main>
     <article class="article">
-      
-   
+
+
       <div class="article__main article__main--width-small container">
         <div class="single-service-block">
-          <h2 data-aos="fade">Methods for Everyone</h2>
-          <p data-aos="fade">Everything along the way, to and from, fascinated her: every pebble, ant, stick, leaf, blade of grass, and crack in the sidewalk was something to be picked up, looked at, tasted, smelled, and shaken. Everything was interesting to her. She knew nothing. I knew everything…been there, done that. She was in the moment, I was in the past. She was mindful. I was mindless.</p>
-          <p data-aos="fade">One touch of a red-hot stove is usually all we need to avoid that kind of discomfort in the future. The same is true as we experience the emotional sensation of stress from our first instances of social rejection or ridicule. We quickly learn to fear and thus automatically avoid potentially stressful situations of all kinds, including the most common of all: making mistakes.</p>
+          <h2 data-aos="fade">General Information</h2>
+          <p data-aos="fade">Jspex Design values privacy of any person or organization, business in nature or otherwise. We are therefore committed to protecting any personal information collected through the designekta.com website Designekta Studios may from time to time change this information and will inform all interested parties of the changes.</p>
         </div>
-        
+
         <div class="single-service-block">
-          <h2 data-aos="fade">Culture</h2>
-          <p data-aos="fade">Everything along the way, to and from, fascinated her: every pebble, ant, stick, leaf, blade of grass, and crack in the sidewalk was something to be picked up, looked at, tasted, smelled, and shaken. Everything was interesting to her. She knew nothing. I knew everything…been there, done that. She was in the moment, I was in the past. She was mindful. I was mindless.</p>
+          <h2 data-aos="fade">Policies</h2>
+          <p data-aos="fade">
+            <h3>Personal Information Collected</h3>
+            jspexdesign.com will gather information in the background when any person or legal entity visits the jspexdesign.com website using cookies. This may include IP address, device type, Internet browser type, operating system, location and other device specific infomation. Data collected is for business intelligence meant for enhancing user experience any time a user visits the jspexdesign.com.
+            <br><br>
+            <h3>Contact</h3>
+             You have the right to review the personal data we keep about you. You can request an overview of your personal data that by emailing info@jspexdesign.com with the subject line Request for personal information. To help us prevent fraudulent collection of personal information, please include a scan of your passport or identity card. If you would like us to remove your personal information from our database, please email info@jspexdesign.com with the subject line Request for removal of personal information. Please bear in mind that we may need to retain certain information for legal and/or administrative purposes such as record keeping or to detect fraudulent or criminal activities.
+          </p>
         </div>
-        <div class="single-service-block">
-          <h2 data-aos="fade">Work</h2>
-          <p data-aos="fade">One touch of a red-hot stove is usually all we need to avoid that kind of discomfort in the future. The same is true as we experience the emotional sensation of stress from our first instances of social rejection or ridicule. We quickly learn to fear and thus automatically avoid potentially stressful situations of all kinds, including the most common of all: making mistakes.</p>
-        </div>
-   
+
+
       </div>
     </article>
 </main>
--- resources/views/front/terms.blade.php
+++ resources/views/front/terms.blade.php
@@ -5,20 +5,25 @@
     <article class="article">
       <div class="article__main article__main--width-small container">
         <div class="single-service-block">
-          <h2 data-aos="fade">Methods for Everyone</h2>
-          <p data-aos="fade">Everything along the way, to and from, fascinated her: every pebble, ant, stick, leaf, blade of grass, and crack in the sidewalk was something to be picked up, looked at, tasted, smelled, and shaken. Everything was interesting to her. She knew nothing. I knew everything…been there, done that. She was in the moment, I was in the past. She was mindful. I was mindless.</p>
-          <p data-aos="fade">One touch of a red-hot stove is usually all we need to avoid that kind of discomfort in the future. The same is true as we experience the emotional sensation of stress from our first instances of social rejection or ridicule. We quickly learn to fear and thus automatically avoid potentially stressful situations of all kinds, including the most common of all: making mistakes.</p>
+          <h2 data-aos="fade">Introduction</h2>
+          <p data-aos="fade">
+            It is highly recommended that you read these Terms and Conditions carefully before using the www.jspexdesign.com website operated by jspexdesign.com14 Digital.
+            <br><br>
+            Your access to and use of the Service is conditioned on your acceptance of and compliance with these Terms. These Terms apply to all visitors, users, and others who access or use the Service.
+            <br><br>
+            By accessing or using the Service you agree to be bound by these Terms. If you disagree with any part of the terms then you may not access the Service. The Terms and Conditions agreement for jspexdesign.com14 Digital has been created by jspexdesign.com.
+          </p>
         </div>
-        
+
         <div class="single-service-block">
-          <h2 data-aos="fade">Culture</h2>
-          <p data-aos="fade">Everything along the way, to and from, fascinated her: every pebble, ant, stick, leaf, blade of grass, and crack in the sidewalk was something to be picked up, looked at, tasted, smelled, and shaken. Everything was interesting to her. She knew nothing. I knew everything…been there, done that. She was in the moment, I was in the past. She was mindful. I was mindless.</p>
+          <h2 data-aos="fade">Link to other websites</h2>
+          <p data-aos="fade">Our Service may contain links to third-party websites or services that are not owned or controlled by jspexdesign.com14 Digital. jspexdesign.com14 Digital has no control over and assumes no responsibility for, the content, privacy policies, or practices of any third-party websites or services. You further acknowledge and agree that jspexdesign.com14 Digital shall not be responsible or liable, directly or indirectly, for any damage or loss caused or alleged to be caused by or in connection with the use of or reliance on any such content, goods, or services available on or through any such web sites or services. We strongly advise you to read the terms and conditions and privacy policies of any third-party websites or services that you visit.</p>
         </div>
         <div class="single-service-block">
-          <h2 data-aos="fade">Work</h2>
-          <p data-aos="fade">One touch of a red-hot stove is usually all we need to avoid that kind of discomfort in the future. The same is true as we experience the emotional sensation of stress from our first instances of social rejection or ridicule. We quickly learn to fear and thus automatically avoid potentially stressful situations of all kinds, including the most common of all: making mistakes.</p>
+          <h2 data-aos="fade">Changes</h2>
+          <p data-aos="fade">We reserve the right, at our sole discretion, to modify or replace these Terms at any time. If a revision is material we will try to provide at least 30 days’ notice prior to any new terms taking effect. What constitutes a material change will be determined at our sole discretion. By continuing to access or use our Service after those revisions become effective, you agree to be bound by the revised terms. If you do not agree to the new terms, please stop using the Service</p>
         </div>
-   
+
       </div>
     </article>
 </main>
